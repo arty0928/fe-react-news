e901181dce54c6e8b2f1fe5d400da081f15fecdb
arty0928 <arty0928@konkuk.ac.kr>
2026-01-13 14:18:10 +0900

    feat: tanstack-query Provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Header } from './components/Header';
 import { LatestNewsSection } from './components/LatestNewsSection';
 import { NewsStand } from './components/NewsStand';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-7xl bg-white shadow-lg">
-        <Header />
-        <LatestNewsSection />
-        <NewsStand />
+    <QueryClientProvider client={queryClient}>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="w-full max-w-7xl bg-white shadow-lg">
+          <Header />
+          <LatestNewsSection />
+          <NewsStand />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
