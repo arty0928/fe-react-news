2df8df568e5dcb7e11072e656f5ebe25a8ebb60b
arty0928 <arty0928@konkuk.ac.kr>
2026-01-13 15:12:42 +0900

    feat: newsView grid에 fetch 한 뉴스 데이터 적용

--- src/news-stand/components/NewsStand.tsx
+++ src/news-stand/components/NewsStand.tsx
@@ -1,6 +1,7 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { fetchNews } from '../../apis/fetchNews';
 import { NewsStandTabs } from './NewsStandTabs';
+import { NewsView } from './NewsView';
 
 export const NewsStand = () => {
   const { data } = useSuspenseQuery({ queryFn: fetchNews, queryKey: ['news'] });
@@ -8,7 +9,7 @@ export const NewsStand = () => {
   return (
     <>
       <NewsStandTabs />
-      {/* <NewsView /> */}
+      <NewsView newsList={data} />
     </>
   );
 };
