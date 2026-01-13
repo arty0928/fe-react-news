3353aa26564c6781580523178e48bc04579a7100
arty0928 <arty0928@konkuk.ac.kr>
2026-01-13 14:17:01 +0900

    style: LatestNewsCard flex, gap

--- src/components/LatestNewsCard.tsx
+++ src/components/LatestNewsCard.tsx
@@ -2,9 +2,9 @@ import type { News } from '../types/news.js';
 
 export const LatestNewsCard = ({ press, title }: News) => {
   return (
-    <article className="border rounded-lg p-4 bg-gray-50">
-      <h3 className="font-bold text-sm mb-2">{press}</h3>
-      <p className="text-sm text-gray-700">{title}</p>
+    <article className="text-sm flex gap-2 align-center border rounded-lg p-4 bg-gray-50">
+      <h3 className="font-bold">{press}</h3>
+      <p className="text-gray-700">{title}</p>
     </article>
   );
 };
