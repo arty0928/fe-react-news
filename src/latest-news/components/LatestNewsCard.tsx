2dc08f239fede2389e73a49316616a7adda34dba
arty0928 <arty0928@konkuk.ac.kr>
2026-01-14 17:47:04 +0900

    style: LatestNewsCard title truncate 및 background color 적용

--- src/latest-news/components/LatestNewsCard.tsx
+++ src/latest-news/components/LatestNewsCard.tsx
@@ -5,9 +5,12 @@ type Props = Omit<News, 'id'> & ComponentProps<'div'>;
 
 export const LatestNewsCard = ({ press, mainTitle, ...rest }: Props) => {
   return (
-    <article {...rest} className="text-sm flex gap-2 align-center border rounded-lg p-4 bg-gray-50">
-      <h3 className="font-bold">{press}</h3>
-      <p className="text-gray-700">{mainTitle}</p>
+    <article
+      {...rest}
+      className="text-sm flex gap-4 items-center align-center border-none rounded-lg p-4 bg-gray-100"
+    >
+      <h3 className="font-bold text-sm">{press}</h3>
+      <p className="truncate text-gray-700">{mainTitle}</p>
     </article>
   );
 };
