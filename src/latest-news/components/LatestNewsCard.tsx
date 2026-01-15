b2ffc6948762dae8251f8d68b3a0101c212daa3c
arty0928 <arty0928@konkuk.ac.kr>
2026-01-15 10:31:05 +0900

    chore: rolling animation 작업중

--- src/latest-news/components/LatestNewsCard.tsx
+++ src/latest-news/components/LatestNewsCard.tsx
@@ -1,11 +1,20 @@
-import type { ComponentProps } from 'react';
+import { useEffect, useRef, type ComponentProps } from 'react';
 import type { News } from '../../types/news';
 
 type Props = Omit<News, 'id'> & ComponentProps<'div'>;
 
 export const LatestNewsCard = ({ press, mainTitle, ...rest }: Props) => {
+  const d = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!d.current) return;
+
+    d.current.classList.add('animation');
+  }, []);
+
   return (
     <article
+      ref={d}
       {...rest}
       className="text-sm flex gap-4 items-center align-center border-none rounded-lg p-4 bg-gray-100"
     >
